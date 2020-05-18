fa7ea22a1bf77949d59e94523ced2879c78934fd
Eduardo Guzman-Diaz <tloz_tlotr@hotmail.com>
2020-05-18 14:53:39 -0400

    Added twitteConfig

---
+++ config.js
@@ -0,0 +1,10 @@
+const twitterConfig = {
+    consumer_key:         '...',
+    consumer_secret:      '...',
+    access_token:         '...',
+    access_token_secret:  '...',
+    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+    strictSSL:            true,     // optional - requires SSL certificates to be valid.
+}
+
+module.exports  = {twitterConfig}
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const Twit = require('twit')
+const {twitterConfig} = require('../config')
+
+const T = new Twit(twitterConfig)
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
