b3bcb068034409410957c3b6f91e7d1cb0210b0f
Eduardo Guzman-Diaz <tloz_tlotr@hotmail.com>
2020-05-18 22:17:50 -0400

    First Tweet!

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,26 @@ const {twitterConfig} = require('../config')
 
 const T = new Twit(twitterConfig)
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+function tweeted(err, data, response){
+  console.log(data)
+}
+function generateTweet(){
+  const tweet = {
+    status : "Job Alert: Engineering Manager @JoinLeague \nhttps://jobs.lever.co/league/ee4f84d4-4f8e-45c3-9b66-a850169bd206 \n #FindJobsTO"
+  }
+  T.post('statuses/update', tweet, tweeted)
+}
+
+
+router.get('/tweet-job', function(req, res, next) {
+  try{
+    generateTweet()
+    res.json({status: 'Tweet Succssful '})
+
+  }catch{
+    res.json({status: 'fail'})
+  }
 });
+
 
 module.exports = router;
