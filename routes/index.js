79ed63b3d31291a70061afd92e2e66fa8c2f709b
Eduardo Guzman-Diaz <tloz_tlotr@hotmail.com>
2020-05-20 14:48:51 -0400

    Updated template + slack

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ function generateTweet(companyName, hashTags = []) {
     const hashTagList = hashTags.join(' ')
     const data = companyData.find((company) => company.name === companyName)
     if (data) {
-        const template = `Job Alert at ${data.name}: Android Engineer ${data.twitterHandle} \nhttps://corp.flipp.com/jobs?gh_jid=2113841 \n ${hashTagList}`
+        const template = `Job Alert at ${data.name}: Software Engineer, IOS ${data.twitterHandle} \n\nhttps://slack.com/intl/en-ca/careers/2087876/software-engineer-ios \n\n ${hashTagList}`
         return template
     } else {
         return null
@@ -34,10 +34,10 @@ router.get('/stop-bot', async function (req, res, next) {
 router.get('/tweet-job', async function (req, res, next) {
     const twitterService = await TwitterServices.fromConfig()
 
-    const hashTags = ['#FindJobsTO', '#Android', '#jobs', "#Engineer", '#toronto']
+    const hashTags = ['#FindJobsTO', '#IOS', '#jobs', "#Engineer", '#toronto', '#Xcode']
 
     try {
-        const tweet = generateTweet('Flipp', hashTags)
+        const tweet = generateTweet('Slack', hashTags)
         if (!tweet) return
         await twitterService.postTweet(tweet)
         res.json(tweet)
