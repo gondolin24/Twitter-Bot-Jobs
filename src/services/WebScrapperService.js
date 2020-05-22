c991c60b84b600129190ab486465369630b505f7
Eduardo Guzman-Diaz <tloz_tlotr@hotmail.com>
2020-05-21 22:34:19 -0400

    Added first webscraper for wealthsimple

---
+++ src/services/WebScrapperService.js
@@ -0,0 +1,40 @@
+const puppeteer = require('puppeteer')
+
+class WebScrapperService {
+
+    async getSoftwareOpenings(page) {
+        const data = await page.evaluate(async () => {
+            const jobPostings = document.querySelectorAll('.posting-title')
+            return  Array.from(jobPostings).map((value) => {
+                const jobName = value.querySelector('h5')
+                const url = value.getAttribute('href')
+                return {
+                    url,
+                    jobName: jobName.textContent
+                }
+            })
+
+        })
+
+        return data.filter((posting) => {
+            const {jobName = ''} = posting
+            return jobName.toLowerCase().includes('software') || jobName.toLowerCase().includes('engineer') || jobName.toLowerCase().includes('engineering') || jobName.toLowerCase().includes('developer')
+        })
+    }
+
+    async getOpeningsAtCompany(jobUrl) {
+        const browser = await puppeteer.launch({headless: true})
+        const page = await browser.newPage()
+        await page.goto(jobUrl)
+
+        const softwareOpenings = await this.getSoftwareOpenings(page)
+        await browser.close()
+        return softwareOpenings
+    }
+
+    static async service() {
+        return new WebScrapperService()
+    }
+}
+
+module.exports = WebScrapperService
