c9d54d7a591848e36abb644b7008b6c5f2b94eaa
Eduardo Guzman-Diaz <tloz_tlotr@hotmail.com>
2020-05-22 22:04:54 -0400

    Adding company service

--- src/services/CompanyService.js
+++ src/services/CompanyService.js
@@ -1,4 +1,24 @@
+const CompanyInfo = require('../data/CompanyInfo')
+
 class CompanyService {
 
+    getCompanyInfo(companyName) {
+        const companyData = CompanyInfo
+        const data = companyData.find((company) => company.name === companyName)
+        return data
+    }
+
+    selectRandomCompanyName() {
+        const availableCompanies = Array.from(CompanyInfo).map((company) => company.name)
+        const randomIndex = Math.floor(Math.random() * availableCompanies.length)
+        return availableCompanies[randomIndex]
+    }
+
+    selectRandomCompany(){
+        const companyName = this.selectRandomCompanyName()
+        return this.getCompanyInfo(companyName)
+    }
 
 }
+
+module.exports = CompanyService
